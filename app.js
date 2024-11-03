784e976773070a0fc34502d48a4f7b5daeb96371
Tran Van Hung <hungtranvan1110@gmail.com>
2024-11-03 11:26:26 +0700

    .

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import homepageRoute from './src/routes/homepage.js';
 import courseRoutes from './src/routes/courseRoutes.js';
 import updatecourse from './src/routes/updatecourse.js';
 import DatabaseConfig from './src/config/databaseConfig.js'; // Nhập lớp kết nối cơ sở dữ liệu
+import jwtMiddleware from './src/middlewares/jwtMiddleware.js'
+import { errorHandler } from './src/errors/errorHandler.js';
 
 const app = express();
 const databaseConfig = new DatabaseConfig(); // Tạo một thể hiện của lớp DatabaseConfig
@@ -24,14 +26,11 @@ databaseConfig.connect();
 // // Route API cho homepage
 // app.use('/api', homepageRoute);
 
-// Route tạo khóa học
+
+app.use(jwtMiddleware)
 app.use('/courses', courseRoutes);
+app.use(errorHandler);
 
-
-// app.use('/updatecourses', updatecourse);
-
-// // Định nghĩa route cho khóa học
-// app.use('/api', khoaHocRoute); // Sử dụng route khóa học
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import { 
-    createCourse
+    createCourse,
+    getCoursesByUserId,
+    updateCourse,
+    getCourseById
 } from '../controllers/khoaHocController.js'; // Nhập các controller
 
 const router = express.Router();
 
-router.post('/create', createCourse);
+// Lấy thông tin các khóa học theo ID người dùng
+router.get('/:userId', getCoursesByUserId);
+
+// Tạo khóa học mới
+router.post('/', createCourse);
+
+// Chỉnh sửa khóa học
+router.patch('/:courseId', updateCourse);
+
+// Lấy thông tin một khóa học theo ID khóa học
+router.get('/:courseId', getCourseById);
+
+router.post('/lessons/:moduleId', upload.single('file'), uploadFile);
+
+router.delete('/lessons/:lessonId', deleteLesson);
+
+router.post('/modules/:courseId', createModule);
+
+router.delete('/modules/:moduleId', deleteLesson);
 
 export default router;
